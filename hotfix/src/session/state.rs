b4190215f7524c1fad1dce15dafd0d5ce0757ea3
David Steiner <david_j_steiner@yahoo.co.nz>
2023-10-24 17:47:25 +0100

    Move writer into session state to ensure it's only set in the correct states

--- hotfix/src/session/state.rs
+++ hotfix/src/session/state.rs
@@ -1,5 +1,9 @@
+use crate::actors::socket_writer::WriterRef;
+use crate::message::parser::RawFixMessage;
+use tracing::{debug, error};
+
 pub enum SessionState {
-    Connected,
+    Connected { writer: WriterRef },
     LoggedOut { reconnect: bool },
     Disconnected { reconnect: bool, reason: String },
 }
@@ -9,6 +13,20 @@ impl SessionState {
         match self {
             SessionState::Disconnected { reconnect, .. } => *reconnect,
             _ => true,
+        }
+    }
+
+    pub async fn send_message(&self, message: RawFixMessage) {
+        match self {
+            Self::Connected { writer } => writer.send_raw_message(message).await,
+            _ => error!("trying to write without an established connection"),
+        }
+    }
+
+    pub async fn disconnect(&self) {
+        match self {
+            Self::Connected { writer } => writer.disconnect().await,
+            _ => debug!("disconnecting an already disconnected session"),
         }
     }
 }
