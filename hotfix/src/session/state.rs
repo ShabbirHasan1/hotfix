ca84b346b3ee9bcae38721d2e6dc2f1f5c011c6c
David Steiner <david_j_steiner@yahoo.co.nz>
2023-10-24 21:46:25 +0100

    Prevent messages from being sent while we're awaiting logon

--- hotfix/src/session/state.rs
+++ hotfix/src/session/state.rs
@@ -3,8 +3,13 @@ use crate::message::parser::RawFixMessage;
 use tracing::{debug, error};
 
 pub enum SessionState {
-    Connected { writer: WriterRef },
+    /// We have established a connection, sent a logon message and await a response.
+    AwaitingLogon { writer: WriterRef, logon_sent: bool },
+    /// The session is active, we have connected and mutually logged on.
+    Active { writer: WriterRef },
+    /// The peer has logged us out.
     LoggedOut { reconnect: bool },
+    /// The TCP connection has been dropped.
     Disconnected { reconnect: bool, reason: String },
 }
 
@@ -16,16 +21,37 @@ impl SessionState {
         }
     }
 
-    pub async fn send_message(&self, message: RawFixMessage) {
+    pub async fn send_message(&mut self, message_type: &[u8], message: RawFixMessage) {
         match self {
-            Self::Connected { writer } => writer.send_raw_message(message).await,
+            Self::Active { writer } => {
+                if message_type == b"A" {
+                    error!("logon message is invalid for active sessions")
+                } else {
+                    writer.send_raw_message(message).await
+                }
+            }
+            Self::AwaitingLogon {
+                writer,
+                ref mut logon_sent,
+            } => {
+                if message_type == b"A" {
+                    if *logon_sent {
+                        error!("trying to send logon twice");
+                    } else {
+                        writer.send_raw_message(message).await;
+                        *logon_sent = true;
+                    }
+                } else {
+                    debug!("received message while in logon state - won't send")
+                }
+            }
             _ => error!("trying to write without an established connection"),
         }
     }
 
     pub async fn disconnect(&self) {
         match self {
-            Self::Connected { writer } => writer.disconnect().await,
+            Self::Active { writer } => writer.disconnect().await,
             _ => debug!("disconnecting an already disconnected session"),
         }
     }
