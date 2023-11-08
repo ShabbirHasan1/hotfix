9177660806c12c2870582ca878ed2bc4c28fbaa9
David Steiner <david_j_steiner@yahoo.co.nz>
2023-11-08 15:07:57 +0000

    Remove further modules from hotfix-encoding

--- crates/hotfix-encoding/src/lib.rs
+++ crates/hotfix-encoding/src/lib.rs
@@ -1,18 +1,13 @@
 //! Crate implementing the encoding (presentation) layer.
 mod buffer;
-pub mod config;
 mod definitions;
-mod error;
 pub mod field_access;
 pub mod field_types;
-pub mod raw_decoder;
-mod utils;
 
 pub use hotfix_derive::FieldType;
 pub use hotfix_dictionary::{self as dict, TagU32};
 
 use buffer::{Buffer, BufferWriter};
-pub use config::{Config, GetConfig};
 #[cfg(feature = "fix42")]
 pub use definitions::fix42;
 pub use definitions::{fix44, HardCodedFixFieldDefinition};
