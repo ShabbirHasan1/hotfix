c81a44a0fb99c3d239aafb2ee0e48eccc4db0c44
David Steiner <david_j_steiner@yahoo.co.nz>
2023-11-08 15:05:16 +0000

    Remove encoder and decoder module

--- crates/hotfix-encoding/src/lib.rs
+++ crates/hotfix-encoding/src/lib.rs
@@ -1,9 +1,7 @@
 //! Crate implementing the encoding (presentation) layer.
 mod buffer;
 pub mod config;
-pub mod decoder;
 mod definitions;
-mod encoder;
 mod error;
 pub mod field_access;
 pub mod field_types;
@@ -15,9 +13,7 @@ pub use hotfix_dictionary::{self as dict, TagU32};
 
 use buffer::{Buffer, BufferWriter};
 pub use config::{Config, GetConfig};
-pub use decoder::{Decoder, Message};
 #[cfg(feature = "fix42")]
 pub use definitions::fix42;
 pub use definitions::{fix44, HardCodedFixFieldDefinition};
-pub use encoder::{Encoder, EncoderHandle, SetField};
 use field_access::FieldType;
